53d115c30636b7d1549833ae8815bc3abbf2c100
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-01-25 16:17:52 -0800

    slight fixes to controller to compensate for the async functions in services
    author: dangkyle64
    date: 1/25/2025

--- controllers/furnitureController.js
+++ controllers/furnitureController.js
@@ -2,7 +2,7 @@ const { getAllFurnituresService, getFurnitureByIdService, addFurnitureService, d
 
 const getAllFurnituresController = async (request, response) => {
     try {
-        const furnitures = getAllFurnituresService();
+        const furnitures = await getAllFurnituresService();
         response.json(furnitures);
     } catch(error) {
         response.status(500).json({ message: 'Error fetching furniture data' });
@@ -18,7 +18,7 @@ const getFurnitureByIdController = async (request, response) => {
             throw new Error('Invalid ID input inside of furnitureByIdController');
         };
 
-        const furniture = getFurnitureByIdService(id);
+        const furniture = await getFurnitureByIdService(id);
 
         if (furniture) {
             response.json(furniture);
@@ -32,27 +32,31 @@ const getFurnitureByIdController = async (request, response) => {
 };
 
 // for tests null value, extreme small or large numbers
-const addFurnitureController = (request, response) => {
+const addFurnitureController = async (request, response) => {
     const newFurniture = request.body;
 
-    if (!newFurniture.type || typeof newFurniture.type !== 'string') {
-        return response.status(400).json({ error: 'Furniture type is required and must be a string.' });
-    };
+    try {
+        if (!newFurniture.type || typeof newFurniture.type !== 'string') {
+            return response.status(400).json({ error: 'Furniture type is required and must be a string.' });
+        };
+    
+        if (!newFurniture.length || !Number.isInteger(newFurniture.length) || newFurniture.length < 0) {
+            return response.status(400).json({ error: 'Furniture length is required and must be a positive integer' });
+        };
+    
+        if (!newFurniture.width || !Number.isInteger(newFurniture.width) || newFurniture.width < 0) {
+            return response.status(400).json({ error: 'Furniture width is required and must be a positive integer' });
+        };
+    
+        if (!newFurniture.height || !Number.isInteger(newFurniture.height) || newFurniture.height < 0) {
+            return response.status(400).json({ error: 'Furniture height is required and must be a positive integer' });
+        };
 
-    if (!newFurniture.length || !Number.isInteger(newFurniture.length) || newFurniture.length < 0) {
-        return response.status(400).json({ error: 'Furniture length is required and must be a positive integer' });
+        const createdFurniture = await addFurnitureService(newFurniture);
+        response.status(201).json(createdFurniture);
+    } catch(error) {
+        response.status(500).json({ error: 'Error creating furniture '});
     };
-
-    if (!newFurniture.width || !Number.isInteger(newFurniture.width) || newFurniture.width < 0) {
-        return response.status(400).json({ error: 'Furniture width is required and must be a positive integer' });
-    };
-
-    if (!newFurniture.height || !Number.isInteger(newFurniture.height) || newFurniture.height < 0) {
-        return response.status(400).json({ error: 'Furniture height is required and must be a positive integer' });
-    };
-
-    const createdFurniture = addFurnitureService(newFurniture);
-    response.status(201).json(createdFurniture);
 };
 
 const updateFurnitureController = async (request, response) => {
@@ -63,7 +67,7 @@ const updateFurnitureController = async (request, response) => {
             throw new Error('Invalid ID input inside of updateFurnitureController');
         };
 
-        const furniture = getFurnitureByIdService(id);
+        const furniture = await getFurnitureByIdService(id);
 
         if (furniture) {
             let updateData = request.body;
@@ -84,7 +88,7 @@ const updateFurnitureController = async (request, response) => {
                 return response.status(400).json({ error: 'Furniture height update must be a valid positive integer' });
             };
 
-            updateFurnitureService(id, updateData);
+            await updateFurnitureService(id, updateData);
             
             response.status(200).send({ message: 'Furniture successfully updated' });
         };
@@ -102,7 +106,7 @@ const deleteFurnitureController = async (request, response) => {
             throw new Error('Invalid ID input inside of deleteFurnitureController');
         };
 
-        const furniture = deleteFurnitureService(id);
+        const furniture = await deleteFurnitureService(id);
 
         if (furniture) {
             response.status(204).send({ message: 'Furniture successfully deleted' });
