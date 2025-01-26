83572ce49cfcd423520ac6ec4b4d8e40abab1b94
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-01-25 17:16:29 -0800

    temporary database hosted on render, expires after a month
    author: dangkyle64
    date: 1/25/2025

--- database.js
+++ database.js
@@ -1,8 +1,13 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite3',
+const sequelize = new Sequelize('postgresql://simulate_room_postgresql_user:MRYgjadY5bCCRLygEe31EVdku1zlW6lo@dpg-cuaok4in91rc738up54g-a.oregon-postgres.render.com/simulate_room_postgresql', {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // For development, but should be true in production
+        },
+      },
     logging: false,
 });
 
