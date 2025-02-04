3cc00fda8869ff67873c36885e4e68328863025a
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-02-04 15:36:09 -0800

    Update imageProcessController.js
    author: dangkyle64
    date: 2/4/2025

--- controllers/imageProcessController.js
+++ controllers/imageProcessController.js
@@ -2,3 +2,6 @@
 // AI model will take image -> convert to 3D Object
 // Options are NeRF from NVIDIA, Pix2Vox, Deep3D, 3D-R2N2
 // start with Pix2Vox or MiDaS
+
+//Python based so have to use child process to call the models
+// python -> load the ai model then child process call with the image
