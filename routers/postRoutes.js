31f47b78ae70177a6f208f46151c5890a3962d5a
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-02-04 13:17:29 -0800

    basic post route to handle the calls from the frontend
    author: dangkyle64
    date: 2/4/2025

--- routers/postRoutes.js
+++ routers/postRoutes.js
@@ -11,5 +11,21 @@ router.post('', (request, response) => {
 });
 
 //post router for images '/image-process' or similar 
+router.post('/image-process', async (request, response) => {
+    try {
+        const { imageData } = request.body;
+        const imagePath = '/tmp/uploaded_image.png';
+
+        // call model and generate the 3D model here
+
+        response.json({
+            success: true,
+            // data: path to the generated 3D Model
+        });
+    } catch(error) {
+        console.error('Error converting image into 3D Object', error);
+        response.status(500).json({ success: false, error: error.message });
+    };
+});
 
 module.exports = router;
