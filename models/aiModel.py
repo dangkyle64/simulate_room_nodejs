df4351bb256d88497a00a61e7208612efc69ff01
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2025-02-07 13:58:44 -0800

    initial setup of loading ai model
    author: dangkyle64
    date: 2/7/2025

--- models/aiModel.py
+++ models/aiModel.py
@@ -1,12 +1,23 @@
-#import model here
+import torch 
+import torch.nn as nn 
+from Pix2Vox.models.encoder import Encoder
+from Pix2Vox.models.decoder import Decoder
+from Pix2Vox.models.merger import Merger 
+from Pix2Vox.models.refiner import Refiner
 
-def load_model():
-    model = 'load the model here'
-    return model
-
-def predict_3D_object(image):
-    model = load_model()
+class Pix2Vox(nn.Module):
+    def __init__(self):
+        super(Pix2Vox, self).__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+        self.merger = Merger()
+        self.refiner = Refiner()
     
-    # generate the 3D object here
-    return 
+    def forward(self, x):
+        features = self.encoder(x)
+        voxel_grid = self.decoder(features)
+        merged_voxels = self.merger(voxel_grid)
+        refined_voxels = self.refiner(merged_voxels)
+        return refined_voxels
 
+model = Pix2Vox()
